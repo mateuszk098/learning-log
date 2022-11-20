3ae3d76f290432e3d96848f8ca9c70124188d3b5
Mateusz Kowalczyk <mkowalczyk013@gmail.com>
2022-11-20 02:11:00 +0100

    Fix typo and braces in urls.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     # Main website.
     path("", views.index, name="index"),
     # Display all topis.
-    path("topic/", views.topics, name="topics"),
+    path("topics/", views.topics, name="topics"),
     # Detail view of the individual topic.
-    path("topic/(<int:topic_id>)", views.topic, name="topic"),
+    path("topics/<int:topic_id>", views.topic, name="topic"),
     # Site for adding a new topic.
     path("new_topic/", views.new_topic, name="new_topic"),
     # Site for adding a new entry.
-    path("new_entry/(<int:topic_id>)", views.new_entry, name="new_entry"),
+    path("new_entry/<int:topic_id>", views.new_entry, name="new_entry"),
     # Site for edditing the existing entry.
-    path("edit_entry/(<int:entry_id>)/", views.edit_entry, name="edit_entry")
+    path("edit_entry/<int:entry_id>/", views.edit_entry, name="edit_entry")
 ]
